4628b6b9d6c5b1641fa209618e06a7415c0b359e
🇦🇿 𝐀𝐙 𝐏𝐑𝐎 𝐌𝐄𝐃𝐘𝐀 ✅️ <azmedyaajans@gmail.com>
2026-03-10 04:33:05 +0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc, query, orderBy } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7kMgr26ETngVhy9VMP1P_IV-X5ac1768",
+  authDomain: "aznew-de931.firebaseapp.com",
+  projectId: "aznew-de931",
+  storageBucket: "aznew-de931.firebasestorage.app",
+  messagingSenderId: "79015742466",
+  appId: "1:79015742466:web:5d0d0636abfabd453b6b1b",
+  measurementId: "G-Z2T7SCTYVT"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export { collection, addDoc, getDocs, deleteDoc, doc, updateDoc, query, orderBy, ref, uploadBytes, getDownloadURL };
